dcb14cbabea4268ce3bdc8d685015e08356ddc34
Maccraft123 <maccraft123mc@gmail.com>
2022-03-17 21:50:28 +0100

    rmenu2: snip away file extensions

--- Rmenu2/main.c
+++ Rmenu2/main.c
@@ -180,9 +180,9 @@ int loadEmulators(struct r_tk *tk)
 
 int loadRomList(struct r_tk *tk, char *ext, char* emu, char* system, char* args)
 {
-	char temp[256];
-	char fancyName[256];
-	char fullPath[256];
+	char temp[256] = "";
+	char fancyName[256] = "";
+	char fullPath[256] = "";
 #ifdef ROS
 	strcpy(temp, "/data/roms/");
 #else
@@ -203,14 +203,10 @@ int loadRomList(struct r_tk *tk, char *ext, char* emu, char* system, char* args)
 		{
 			if(ent->d_type == DT_REG)
 			{
-//				if(!!(strcmp(strrchr(ent->d_name, '\0') - 3, ext)))
-//					continue; // wrong extension
-
 				if(i == 0)
 					new_tab(tk, system);
-//				strncpy(fancyName, ent->d_name, strlen(ent->d_name) - (1+strlen(ext)));
-//				new_btn(tk, tk->tabHead, fancyName, 0, 0);
-				new_btn(tk, tk->tabHead, ent->d_name, 0, 0);
+				strncpy(fancyName, ent->d_name, strcspn(ent->d_name, "."));
+				new_btn(tk, tk->tabHead, fancyName, 0, 0);
 				tmp = malloc(sizeof(struct btnData));
 				tmp->type = rom;
 				strcpy(tmp->emu, emu);
